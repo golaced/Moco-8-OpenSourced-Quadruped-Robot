8007c84edb1f8b560cfababf1e07a0ccda8b1e0d
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-07 10:06:04 +0800

    Delete firmware directory

--- firmware/遥控器/User/time.h
+++
@@ -1,9 +0,0 @@
-#ifndef _TIME_H__
-#define _TIME_H__
-#include "stm32f10x_rcc.h"
-#include "stm32f10x_gpio.h"
-#include "stm32f10x_tim.h"
-
-void Time4ON(void);
-extern u8 flag_ms[100];
-#endif
--- firmware/遥控器/User/usart/usart_config.h
+++
@@ -1,7 +0,0 @@
-#ifndef __USART_CONFIG_H
-#define	__USART_CONFIG_H
-
-#define TEST_USART								USART1
-
-
-#endif /* __USART_CONFIG_H */
